9f561fa36dae591baa4241de2f70870ff0af3920
belvis <elvishernandez535@gmail.com>
2018-01-21 22:01:11 -0700

    Search bar exists

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class Search extends Component{
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
         this.state = {
-            searchTerm: ''
+          searchTerm: ''
         }
     }
+    
 
     searchResult(){
         axios.get()
@@ -17,4 +18,27 @@ class Search extends Component{
             console.error(err);
         })
     }
+    render(){
+      return (
+        <div>
+          <input
+          onChange=""
+          placeholder="Search Hero"/>
+          <button>Search</button>
+        </div>
+      )
+    }
 }
+export default Search;
+// marvel.characters.findByName(searchTerm)
+//   .then(function(res) {
+//     console.log('Found character ID', res.data[0].id);
+//     return marvel.characters.comics(res.data[0].id);
+//   })
+//   .then(function(res) {
+//     console.log('found %s comics of %s total', res.meta.count, res.meta.total);
+//     console.log(res.data);
+//   })
+//   .fail(console.error)
+//   .done();
+//   export default Search;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import SearchBar from './components/Search_bar';
 // import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
+
+// marvel.characters.findAll(20,340)
+//   .then(console.log)
+//   .fail(console.error)
+//   .done();
